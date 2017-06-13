38951553405a56906de9221733449cdc5ac58c2f
王志成 <wangzhicheng@tongxinyiliao.com>
2017-06-13 11:00:40 +0800

    iris

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"tableStore/api"
 	"tableStore/lib"
+	"tableStore/models"
 
 	"github.com/aliyun/aliyun-tablestore-go-sdk/tablestore"
+	"github.com/kataras/iris/context"
+
+	"github.com/kataras/iris"
 )
 
 func init() {
@@ -16,9 +17,45 @@ func init() {
 }
 
 func main() {
-	http.HandleFunc("/dicom", api.DICOMHandler)
+	app := iris.New()
+
+	// Get 读取数据
+	app.Get("/dicom/{code}", func(ctx context.Context) {
+		code := ctx.Params().Get("code")
+		var result *lib.Result
+		film, _ := models.GetOneFilm(code)
+		result = &lib.Result{Success: true, Data: film, Code: code}
+		ctx.JSON(&result)
+	})
+
+	// Add 新增数据
+	app.Post("/dicom", func(ctx context.Context) {
+		var result *lib.Result
+		film := models.Film{}
+		ctx.ReadForm(&film)
+		//
+		if film.Code == "" {
+			result = &lib.Result{Success: false, Msg: "缺少code参数"}
+		} else {
+			code := models.AddOneFilm(film) //
+			result = &lib.Result{Success: true, Code: code, Msg: "保存成功"}
+		}
+		ctx.JSON(&result)
+	})
+
+	// Delete 删除数据
+	app.Delete("/dicom/{code}", func(ctx context.Context) {
+		code := ctx.Params().Get("code")
+		var result *lib.Result
+		err := models.DeleteOneFilm(code)
+		if err != nil {
+			result = &lib.Result{Success: false, Code: code, Msg: "删除错误: " + err.Error()}
+		} else {
+			result = &lib.Result{Success: true, Code: code, Msg: "删除成功"}
+		}
+		ctx.JSON(&result)
+	})
 
 	// 启动
-	fmt.Println("starting server on port ", lib.Conf.Port, "...")
-	http.ListenAndServe(":"+lib.Conf.Port, nil)
+	app.Run(iris.Addr(":" + lib.Conf.Port))
 }
